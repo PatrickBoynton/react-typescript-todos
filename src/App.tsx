06cfbb27fdd1e6b374469d0dd4df033a2507ae7e
Stephen Boynton <stephen.boynton@somoglobal.com>
2021-08-03 09:17:04 -0400

    changes with comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,26 @@ interface Todo {
 }
 
 const App = (): ReactElement => {
-    const [input, setInput] = useState({});
+    const [input, setInput] = useState('');
     const [todos, setTodos] = useState<Todo[]>([]);
-    const [todo, setTodo] = useState<Todo | null>(null);
     const [alert, setAlert] = useState<string>('');
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): ChangeEvent<HTMLInputElement> => {
-        setInput({
-            ...input,
-            id: Math.round(Math.random() * 1000),
-            [event.target.name]: event.target.value,
-            complete: false,
-        });
+        // the input should remain a string value until it's ready to be used
+        setInput(event.target.value);
         return event;
     };
 
-    const onAddTodo = (todo: any) => {
-        if (todo['title']) {
-            setTodos([todo, ...todos]);
+
+    const onAddTodo = () => {
+        if (input) {
+            // create new ToDo when they click add, using the input
+            const newTodo = {
+                id: Math.round(Math.random() * 1000),
+                title: input,
+                complete: false,
+            };
+            setTodos([newTodo, ...todos]);
         } else {
             onAddAlert();
         }
@@ -35,6 +37,7 @@ const App = (): ReactElement => {
         setTodos(todos.filter((todo) => todo.id !== id));
     };
 
+    // nice
     const onAddAlert = (): void => {
         setAlert('Please add a title.');
         setTimeout(() => {
@@ -42,16 +45,38 @@ const App = (): ReactElement => {
         }, 3000);
     };
 
+    const clearInput = () => {
+        setInput('');
+    };
+
     const handleSubmit = (event: FormEvent): void => {
+        // the value will be coming from 'input' which you are tracking with state,
+        // so the only thing you need the event for is to prevent a page refresh.
         event.preventDefault();
-        onAddTodo(input);
+        onAddTodo();
+        // after submit, your input is reset and ready for the next.
+        clearInput();
     };
 
+    // be careful with your naming you have a few functions with the name 'todo' and that can be confusing
     const changeComplete = (todo: Todo): void => {
-        todo.complete = !todo.complete;
-        setTodo({ id: todo.id, title: todo.title, complete: todo.complete });
+        // hook functions can take a callback that receives the old state as an argument.
+        // this helps to prevent stale state when updating.
+        // This version takes the old state, selects the intended todo, marks it complete, then returns the updated list.
+        setTodos((prevTodos) => {
+            // use your ids to select your correct todo
+            const selectedTodo = prevTodos.find((prevTodo) => prevTodo.id === todo.id);
+            if(selectedTodo){
+            // remove old todo from the previous list
+                const filteredTodos = prevTodos.filter((prevTodo) => prevTodo.id !== todo.id);
+                return [...filteredTodos, { ...selectedTodo, complete: true }];
+            }
+            // if there is no todo with the selected id, we're in trouble haha
+            return prevTodos;
+        });
     };
 
+    // There can only be one H1 per webpage for accessibility reasons. The h1, h2, h3 tags shouldn't be used for style, only for creating an outline on the page.
     const todoDisplay = todos.map((todo) => (
         <>
             <div className="display">
@@ -59,6 +84,7 @@ const App = (): ReactElement => {
                     key={todo.id}
                     onClick={() => changeComplete(todo)}
                     style={
+                        // nice
                         todo.complete
                             ? { textDecoration: 'line-through' }
                             : { textDecoration: 'none' }
@@ -78,7 +104,7 @@ const App = (): ReactElement => {
                 <h2>Add a Todo</h2>
                 <label htmlFor="title">Title</label>
                 <span className={alert ? 'alert' : ''}>{alert}</span>
-                <input type="text" name="title" onChange={handleChange} />
+                <input type="text" name="title" value={input} onChange={handleChange} />
                 <button type="submit">Add Todo</button>
             </form>
 
