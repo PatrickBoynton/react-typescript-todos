b511619136ea5d43dfe98994dd5af44f81a85029
PatrickBoynton <jpboynton3@gmail.com>
2021-08-04 15:36:25 -0400

    Moved the todo display here.

---
+++ src/components/TodoDisplay.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { Todo } from '../models/Todo';
+
+interface Props {
+    todo: Todo;
+    changeComplete: (todo: Todo) => void;
+    onDelete: (id: Todo['id']) => void;
+}
+
+const TodoDisplay: FC<Props> = ({changeComplete, todo, onDelete}: Props) => {
+    return (
+        <>
+            <div className="display">
+                <h1
+                    key={ todo.id }
+                    onClick={ () => changeComplete(todo) }
+                    style={
+                        todo.complete
+                            ? {textDecoration: 'line-through'}
+                            : {textDecoration: 'none'}
+                    }>
+                    { todo.title }
+                </h1>
+                <button className="delete" onClick={ () => onDelete(todo.id) }>
+                    Delete
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default TodoDisplay;
