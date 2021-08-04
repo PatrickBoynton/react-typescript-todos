44576a911f3f6c6025fad55b1a531714212a25b2
PatrickBoynton <jpboynton3@gmail.com>
2021-08-04 15:35:52 -0400

    Created a form component.

---
+++ src/components/Form.tsx
@@ -0,0 +1,21 @@
+import React, { ChangeEvent, FC, FormEvent } from 'react';
+
+interface Props {
+    handleSubmit: (event: FormEvent) => void;
+    handleChange: (event: ChangeEvent<HTMLInputElement>) => ChangeEvent<HTMLInputElement>;
+    alert: string;
+}
+
+const Form: FC<Props> = ({handleSubmit, handleChange, alert}: Props) => {
+    return (
+        <form onSubmit={ handleSubmit }>
+            <h2>Add a Todo</h2>
+            <label htmlFor="title">Title</label>
+            <span className={ alert ? 'alert' : '' }>{ alert }</span>
+            <input type="text" name="title" onChange={ handleChange } />
+            <button type="submit">Add Todo</button>
+        </form>
+    );
+};
+
+export default Form;
