8b4673d1af304138f5a2b4be32c546db9b2f8214
PatrickBoynton <jpboynton3@gmail.com>
2021-08-07 22:15:12 -0400

    Added the new toast component.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, FC, FormEvent } from 'react';
 import './Form.css';
+import Toast from '../Toast/Toast';
 
 interface Props {
     handleSubmit: (event: FormEvent) => void;
@@ -8,12 +9,16 @@ interface Props {
 }
 
 const Form: FC<Props> = ({handleSubmit, handleChange, alert}: Props) => {
+    // const alertDisplay = <Toast alert={ alert }>Test</Toast>;
     return (
         <form onSubmit={ handleSubmit }>
             <h2>Add a Todo</h2>
             <label htmlFor="title">Title</label>
-            <span className={ alert ? 'alert' : '' }>{ alert }</span>
+            <Toast alert={alert}>Please input a title.</Toast>
             <input type="text" name="title" onChange={ handleChange } />
+            <label htmlFor="category">Category</label>
+            <Toast alert={alert}>Please give a category.</Toast>
+            <input type="text" name="category" onChange={ handleChange } />
             <button type="submit">Add Todo</button>
         </form>
     );
